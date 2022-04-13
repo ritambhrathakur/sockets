d13837219dae185d04dba2927b0dbca594401098
ritambhrathakur <ritambhra.thakur@softuvo.in>
2022-04-13 17:40:28 +0530

    routers and handle messages

--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 
+from chat.models import ChatMessages
+
 # Create your views here.
 # def index(request):
 #     return render(request, 'chat/index.html')
 
 def index(request):
-    return render(request, 'chat/index.html', {})
+    # groups=ChatMessages.objects.all()
+    res = set(ChatMessages.objects.values_list('text_name', flat=True))
+    print(res)
+    context= {"group":res}
+    return render(request, 'chat/index.html', context)
 
 def room(request, room_name):
     return render(request, 'chat/room.html', {
--- mysite/asgi.py
+++ mysite/asgi.py
@@ -1,19 +1,47 @@
 
 # mysite/asgi.py
+# import os
+
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from django.core.asgi import get_asgi_application
+# import chat.routing
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+# application = ProtocolTypeRouter({
+#   "http": get_asgi_application(),
+#   "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             chat.routing.websocket_urlpatterns
+#         )
+#     ),
+# })
+
 import os
+# from . import consumers
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.conf.urls import url
 from django.core.asgi import get_asgi_application
-import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+from chat.consumers import ChatConsumer
 
 application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": django_asgi_app,
+
+    # WebSocket chat handler
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            url(r'ws/chat/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
+            # url(r"^chat/$", PublicChatConsumer.as_asgi()),
+        ])
     ),
 })
